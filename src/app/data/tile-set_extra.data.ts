1f50dadd6b577d38bc397a8ea8fabd53475ebabd
Denis Zholob <dddgamer3x@gmail.com>
2024-01-23 20:15:18 -0700

    Add Editor Page

---
+++ src/app/data/tile-set_extra.data.ts
@@ -0,0 +1,24 @@
+import { TileSetBase } from '../classes/TileSet.model.js';
+
+export const TILE_SETS_EXTRA_BASE: TileSetBase[] = [
+  {
+    name: 'dune-cliffs',
+    // link: 'https://game-endeavor.itch.io/mystic-woods',
+    set: [
+      [28, 112, 247, 223, 124, 124, 124, 31, 241],
+      [7, 193, 253, 127, 199, 199, 199, 31, 241],
+      [30, 240, 60, 120, 255, 119, 221, 31, 241],
+      [15, 225, 135, 195, 199, 199, 199, 112, 7],
+    ],
+  },
+  {
+    name: 'DDD',
+    // link: 'https://game-endeavor.itch.io/mystic-woods',
+    set: [
+      [28, 124, 112, 16, 20, 92, 116, 80],
+      [31, 255, 241, 17],
+      [7, 199, 193, 1],
+      [0, 4, 68, 64, 0],
+    ],
+  },
+];
